69c6e7b0c928a8ef21097f967b862a802bbc278a
Evstafev Nikita <evstafev.nik@yandex.ru>
2020-12-12 22:05:58 +0800

    Добавлено считывание данных из Firebase

--- app/src/main/java/com/example/to_doandroid/MainActivity.java
+++ app/src/main/java/com/example/to_doandroid/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.to_doandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.Layout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -15,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView titlePage, subTitle, versionTitle;
 
-    DatabaseReference reference;
+    DatabaseReference reference; // Создаем DBReference для считывания и записи данных в Firebase
     RecyclerView taskList; // Список с задачами
     ArrayList<Task> tasks; // ArrayList с тасками
     TaskAdapter taskAdapter;
@@ -26,18 +34,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // получаем по id
-        titlePage = findViewById(R.id.titlePage);
-        subTitle = findViewById(R.id.subTitle);
-        versionTitle = findViewById(R.id.versionTitle);
+        this.titlePage = findViewById(R.id.titlePage);
+        this.subTitle = findViewById(R.id.subTitle);
+        this.versionTitle = findViewById(R.id.versionTitle);
 
         // добавляем свой шрифт
-        /*Typeface TekoLight = Typeface.createFromAsset(getAssets(), "fonts/tekomedium.ttf");
-        Typeface TekoMedium = Typeface.createFromAsset(getAssets(), "fonts/tekomedium.ttf");
+      /*  Typeface TekoLight = Typeface.createFromAsset(getAssets(), "res/font/tekomedium.ttf");
+        Typeface TekoMedium = Typeface.createFromAsset(getAssets(), "res/font/tekomedium.ttf");
 
         // Меняем шрифт у текста на главной панели
+        this.titlePage.setTypeface(TekoMedium);
+        this.subTitle.setTypeface(TekoMedium);
+        this.versionTitle.setTypeface(TekoLight);*/
 
-        titlePage.setTypeface(TekoMedium);
-        subTitle.setTypeface(TekoMedium);
-        versionTitle.setTypeface(TekoLight);*/
+        this.taskList = findViewById(R.id.taskList);
+        this.taskList.setLayoutManager(new LinearLayoutManager(this));
+        this.tasks = new ArrayList<Task>();
+
+        //Получаем данные из Firebase
+        reference = FirebaseDatabase.getInstance().getReference().child("TO-DOAndroid"); //название главного узла в Firebase
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Извлекаем данные из узла
+                for (DataSnapshot dataSnapshot1: dataSnapshot.getChildren()) { //получили всех потомков
+                    Task task = dataSnapshot1.getValue(Task.class); // получили значение
+                    tasks.add(task); // добавили в список задач
+                }
+                taskAdapter = new TaskAdapter(MainActivity.this, tasks);
+                taskList.setAdapter(taskAdapter);
+                taskAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(getApplicationContext(), "No data", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
