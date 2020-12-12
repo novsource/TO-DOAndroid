6f7718060e5b3d9b3575bf05eea36dfce8ce0ba4
Evstafev Nikita <evstafev.nik@yandex.ru>
2020-12-12 17:13:18 +0800

    Добавлены элементы на главный экран: название программы, RecyclerView и версия программы

--- app/src/main/java/com/example/to_doandroid/MainActivity.java
+++ app/src/main/java/com/example/to_doandroid/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.to_doandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView titlePage, subTitle, versionTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // получаем по id
+        titlePage = findViewById(R.id.titlePage);
+        subTitle = findViewById(R.id.subTitle);
+        versionTitle = findViewById(R.id.versionTitle);
+
+        // добавляем свой шрифт
+        /*Typeface TekoLight = Typeface.createFromAsset(getAssets(), "fonts/tekomedium.ttf");
+        Typeface TekoMedium = Typeface.createFromAsset(getAssets(), "fonts/tekomedium.ttf");
+
+        // Меняем шрифт у текста на главной панели
+
+        titlePage.setTypeface(TekoMedium);
+        subTitle.setTypeface(TekoMedium);
+        versionTitle.setTypeface(TekoLight);*/
     }
 }
